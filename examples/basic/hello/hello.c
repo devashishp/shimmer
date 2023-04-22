f5038a272b9959dc6ec1ed17641fee1b61778aec
Dev <dpuranda@ucsc.edu>
2023-04-21 17:19:37 -0700

    fputs wrapper

--- examples/basic/hello/hello.c
+++ examples/basic/hello/hello.c
@@ -1,12 +1,19 @@
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 int main(void) {
   char *buf = "Hello!\n";
   write(1, buf, strlen(buf));
-  int f1 = open("testfile.txt", O_CREAT | O_WRONLY);
+  int f1 = open("testfile.txt", O_CREAT | O_WRONLY | O_TRUNC);
   write(f1, "Another print statement", 23);
   close(f1);
+  FILE *f2 = fopen("AnotherTest.txt", "w+");
+  int result = fputs("Testing fputs tracing here", f2);
+  if (result < 0) {
+    printf("Error!\n");
+  }
+  fclose(f2);
   return 0;
 }
--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -1,4 +1,5 @@
 use shimmer::{shimmer, shimmer_hook};
+use std::ffi::CStr;
 use std::fs::read_link;
 use std::path::PathBuf;
 
@@ -20,6 +21,8 @@ trait BasicIO {
         buf: *mut libc::c_void,
         nbytes: libc::size_t,
     ) -> libc::c_int;
+
+    unsafe fn fputs(&mut self, buf: *mut libc::c_char, filestruct: *mut libc::FILE) -> libc::c_int;
 }
 
 #[shimmer_hook]
@@ -70,5 +73,31 @@ impl BasicIO for State {
         };
         // Doesn't !!
         //      println!("[write] fd={fd}, size={nbytes}");
+    }
+
+    unsafe fn fputs(&mut self, buf: *mut libc::c_char, filestruct: *mut libc::FILE) -> libc::c_int {
+        let c_str: &CStr = unsafe { CStr::from_ptr(buf) };
+        let fd: libc::c_int = unsafe {
+            let fd = libc::fileno(filestruct);
+            if fd.is_negative() {
+                panic!("Invalid FD");
+            }
+            fd
+        };
+        let path_fd = PathBuf::from(format!("/proc/self/fd/{}", fd));
+        let file_name = read_link(path_fd).unwrap();
+        let msg = format!(
+            "[write], fd={fd}, path={} size={}\n",
+            file_name.display(),
+            c_str.to_str().unwrap().len()
+        );
+        let _x = unsafe {
+            libc::syscall(
+                libc::SYS_write,
+                0,
+                msg.as_bytes().as_ptr() as usize,
+                msg.len(),
+            )
+        };
     }
 }
