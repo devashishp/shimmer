960d756a39ff2eac436fae37e9e2d1200d6b4f6f
Dev <dpuranda@ucsc.edu>
2023-04-23 20:53:59 -0700

    Up to date with shimmer macros

--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -1,4 +1,4 @@
-use shimmer::{shimmer, shimmer_hook};
+use shimmer::prelude::*;
 use std::ffi::CStr;
 use std::fs::read_link;
 use std::path::PathBuf;
@@ -35,19 +35,10 @@ impl BasicIO for State {
     ) -> libc::c_int {
         let path_fd = PathBuf::from(format!("/proc/self/fd/{}", fd));
         let file_name = read_link(path_fd).unwrap();
-        let msg = format!(
-            "[read], fd={fd}, path={} size={nbytes}\n",
+        shimmer_println!(
+            "[read], fd={fd}, path={} size={nbytes}",
             file_name.display()
         );
-        let _x = unsafe {
-            libc::syscall(
-                libc::SYS_write,
-                0,
-                msg.as_bytes().as_ptr() as usize,
-                msg.len(),
-            )
-        };
-        //        println!("[read] fd={fd}, size={nbytes}");
     }
 
     unsafe fn write(
@@ -59,20 +50,10 @@ impl BasicIO for State {
         // Works!!
         let path_fd = PathBuf::from(format!("/proc/self/fd/{}", fd));
         let file_name = read_link(path_fd).unwrap();
-        let msg = format!(
-            "[write], fd={fd}, path={} size={nbytes}\n",
+        shimmer_println!(
+            "[write], fd={fd}, path={} size={nbytes}",
             file_name.display()
         );
-        let _x = unsafe {
-            libc::syscall(
-                libc::SYS_write,
-                0,
-                msg.as_bytes().as_ptr() as usize,
-                msg.len(),
-            )
-        };
-        // Doesn't !!
-        //      println!("[write] fd={fd}, size={nbytes}");
     }
 
     unsafe fn fputs(&mut self, buf: *mut libc::c_char, filestruct: *mut libc::FILE) -> libc::c_int {
@@ -86,18 +67,10 @@ impl BasicIO for State {
         };
         let path_fd = PathBuf::from(format!("/proc/self/fd/{}", fd));
         let file_name = read_link(path_fd).unwrap();
-        let msg = format!(
-            "[write], fd={fd}, path={} size={}\n",
+        shimmer_println!(
+            "[write], fd={fd}, path={} size={}",
             file_name.display(),
             c_str.to_str().unwrap().len()
         );
-        let _x = unsafe {
-            libc::syscall(
-                libc::SYS_write,
-                0,
-                msg.as_bytes().as_ptr() as usize,
-                msg.len(),
-            )
-        };
     }
 }
